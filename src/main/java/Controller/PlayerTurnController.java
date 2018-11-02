26f8a4d2ec6bbc76dba6c074a04d6341bfa0a038
Nicolai Nisbeth <nicolai.nisbeth@hotmail.com>
2018-11-02 15:17:24 +0100

    PlayerTurnController

--- src/main/java/Controller/PlayerTurnController.java
+++ src/main/java/Controller/PlayerTurnController.java
@@ -1,4 +1,13 @@
 package Controller;
 
+import Model.Player;
+
 public class PlayerTurnController {
+
+
+    public void roll(DiceCupController diceCup, Player player, SquareController square){
+        square.newSquare(player.getPosition(),diceCup.rollAndGetSum());
+    }
+
+
 }
