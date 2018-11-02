2acc8d8b58f7b8d52108364b76097142e200c161
theogutt <theodorguttesen@gmail.com>
2018-11-02 18:03:28 +0100

    arbejdede på account og accountcontroller. De er ikke færdige.

--- src/main/java/Controller/AccountController.java
+++ src/main/java/Controller/AccountController.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import Model.Account;
+
 public class AccountController {
+    Account account;
+    public AccountController() {
+        account = new Account(1000);
+    }
+    public int getBalance()
+    {
+        return getBalance();
+    }
+    public int resetBalance(){
+        return resetBalance();
+    }
 }
--- src/main/java/Model/Account.java
+++ src/main/java/Model/Account.java
@@ -1,4 +1,20 @@
 package Model;
 
 public class Account {
+    int balance;
+    public Account(int balancetal){
+        this.balance=balancetal;
+    }
+    public void deposit(int amount){
+        balance=balance+amount;
+    }
+    public void withdraw(int amount){
+        balance=balance-amount;
+    }
+    public int getBalance(){
+        return balance;
+    }
+    public int resetBalance(){
+        return balance=0;
+    }
 }
