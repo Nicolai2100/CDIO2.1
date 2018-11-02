16dc821e5ccb0e0df4821e1f1e0b2fe6595eb0e7
Nicolai Nisbeth <nicolai.nisbeth@hotmail.com>
2018-11-02 15:26:34 +0100

    get og set til PlayerTurnController

--- src/main/java/Controller/PlayerTurnController.java
+++ src/main/java/Controller/PlayerTurnController.java
@@ -4,10 +4,30 @@ import Model.Player;
 
 public class PlayerTurnController {
 
+    private Player model;
+    private SquareController square;
 
-    public void roll(DiceCupController diceCup, Player player, SquareController square){
+    public PlayerTurnController(Player model, SquareController square){
+        this.model = model;
+        this.square = square;
+    }
+
+    public void roll(DiceCupController diceCup, Player player){
         square.newSquare(player.getPosition(),diceCup.rollAndGetSum());
     }
+
+    public String getName() {
+        return model.getName();
+    }
+
+    public void setName(String name) {
+        model.setName(name);
+    }
+
+    public int getBalance() {
+        return model.getBalance();
+    }
+
 
 
 }
