23b944bef33305a695be1a452cc080f2ed16168b
Nicolai <nicolailarsen2100@gmail.com>
2018-11-03 16:12:25 +0100

    Play again virker ikke - for meget arbejde - Ændret playerTurnController ref til playerTC

--- src/main/java/Controller/GameEngine.java
+++ src/main/java/Controller/GameEngine.java
@@ -5,8 +5,8 @@ import View.Gui_Handler;
 
 public class GameEngine {
     private Gui_Handler guiHandler;
-    private PlayerTurnController playerTurnController1;
-    private PlayerTurnController playerTurnController2;
+    private PlayerTurnController playerTC1;
+    private PlayerTurnController playerTC2;
     private Player player1;
     private Player player2;
     private SquareController square;
@@ -15,8 +15,8 @@ public class GameEngine {
         guiHandler = new Gui_Handler();
         player1 = new Player();
         player2 = new Player();
-        playerTurnController1 = new PlayerTurnController(player1, square);
-        playerTurnController2 = new PlayerTurnController(player2, square);
+        playerTC1 = new PlayerTurnController(player1, square);
+        playerTC2 = new PlayerTurnController(player2, square);
         square = new SquareController();
     }
     public void start(){
@@ -27,12 +27,15 @@ public class GameEngine {
         //int noPlayers = guiHandler.askForNoPlayers();
         //LOOP and make players
         //guiHandler.askForPlayerNames();
-        guiHandler.setGameUpGui(playerTurnController1, playerTurnController2);
+        guiHandler.setGameUpGui(playerTC1, playerTC2);
     }
     public void playAgain(){
             String svar = guiHandler.playAgainGui();
-        if (svar.equals("ja"))
+        if (svar.equals("ja")){
+            guiHandler.removePlayer1Car(playerTC1);
+            guiHandler.removePlayer2Car(playerTC2);
             start();
+        }
         else{
             System.exit(1);
         }
@@ -47,7 +50,7 @@ public class GameEngine {
         guiHandler.startGameGui(message);
         //Set player names
         guiHandler.enterNamePlayer(message, playerTurnController1, playerTurnController2);
-        //guiHandler.enterNamePlayer(message, playerTurnController2);
+        //guiHandler.enterNamePlayer(message, playerTC2);
 
      //Start the main game
         do {
@@ -55,15 +58,15 @@ public class GameEngine {
             guiHandler.removePlayer1Car(playerTurnController1);
             playerTurnController1.roll(diceCup, playerTurnController1);
             guiHandler.player1TurnUpdate(playerTurnController1, diceCup.getRef(0),diceCup.getRef(1));
-           /* if (player1.getWon()){
+            if (player1.getWon()){
                 break;
-            }*/
+            }
 
             guiHandler.playerTurnGui(message, playerTurnController2);
             guiHandler.removePlayer2Car(playerTurnController2);
             playerTurnController2.roll(diceCup, playerTurnController2);
             guiHandler.player2TurnUpdate(playerTurnController2, diceCup.getRef(0),diceCup.getRef(1));
-            if (player2.getWon()){
+           if (player2.getWon()){
                 break;
             }
         }
@@ -72,7 +75,7 @@ public class GameEngine {
 
         guiHandler.playerWonGui(message, playerTurnController1, playerTurnController2);
 
-        playAgain();
+        //playAgain();
 
     }
 
