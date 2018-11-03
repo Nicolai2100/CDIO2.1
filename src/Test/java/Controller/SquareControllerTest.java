099f74b7129c682df82a317033138e2d204204e0
NicolaiNisbeth <nicolai.nisbeth@hotmail.com>
2018-11-03 16:54:38 +0100

    added test to squareImpact

--- src/Test/java/Controller/SquareControllerTest.java
+++ src/Test/java/Controller/SquareControllerTest.java
@@ -23,18 +23,20 @@ public class SquareControllerTest {
             int newPlayerPosition = player.getPosition();
             assertTrue(1 <= newPlayerPosition && newPlayerPosition <= 12 );
         }
-
-
-
-
-
     }
-
+    
     @Test
     public void squareImpact() {
-        player.setPosition(2);
-        square.squareImpact(player, diceCup);
-        int x = player.getBalance();
-        assertEquals(x, 1250);
+        int[] position = {1,2,3,4,5,6,7,8,9,11,12};
+        int[] fieldList = {0,250,-100,100,-20,180,0,-70,60,-50,650};
+
+        for (int i = 0; i < position.length; i++) {
+            int initialBalance = player.getBalance();
+            player.setPosition(position[i]);
+            square.squareImpact(player, diceCup);
+            int balancePostImpact = player.getBalance();
+            assertEquals(balancePostImpact, initialBalance+fieldList[i]);
+        }
+
     }
 }
